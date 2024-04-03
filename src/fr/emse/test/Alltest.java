9f442616fb073fd94964c6beafee26b2b5ea9e07
HarentsoaR <randriamaholimanana1@gmail.com>
2024-04-03 21:02:12 +0300

    Tp terminer

---
+++ src/fr/emse/test/Alltest.java
@@ -0,0 +1,14 @@
+package fr.emse.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    MoneyTest.class,
+    MoneyBagTest.class
+})
+public class Alltest {
+
+}
